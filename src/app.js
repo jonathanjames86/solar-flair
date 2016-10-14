26f560239fc4d91381c3333479bd40f73ed8a87c
James Putnam <jonathan.putnam86@gmail.com>
2016-10-14 00:58:07 -0600

    basic format set-up

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, browserHistory, hashHistory, DefaultRoute } from "react-router";
+import routes from './routes';
+import Landing from './pages/Landing/Landing';
+
+
+ReactDOM.render(
+    <Router history={browserHistory} routes={routes}>
+    </Router>,
+  document.getElementById('solar'));
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, IndexRoute, IndexRedirect } from "react-router";
+import Landing from './pages/Landing/Landing';
+
+export default (
+
+  <Route path="/" component={Landing} >
+  </Route>
+);
